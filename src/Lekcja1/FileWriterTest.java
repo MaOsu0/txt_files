5a09450911f7f050844fd37587919d5fa38ea631
MaOsu0 <mariusz.osuch1@gmail.com>
2022-03-19 12:49:54 +0100

    19.03.2022

--- src/Lekcja1/FileWriterTest.java
+++ src/Lekcja1/FileWriterTest.java
@@ -1,7 +1,26 @@
 package Lekcja1;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 class FileWriterTest {
     public static void main(String[] args) {
+        String fileName = "testFile.txt";
+        try (
+                var fileWriter = new FileWriter(fileName, true);
+                var bufferedWriter = new BufferedWriter(fileWriter);
+        ) {
+            bufferedWriter.write("Bolek");
+            bufferedWriter.newLine();
+            bufferedWriter.write("Lolek");
+            bufferedWriter.newLine();
+            bufferedWriter.write("Karolek");
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
